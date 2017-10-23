50a333333a1ca525bd0075d6ab1991794926fa9d
Gustavo Oliveira <gusttavo@live.com>
2017-10-23 19:36:08 -0200

    Atualização

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 var menu = document.querySelector(".menu");
+window.onscroll = function() {fixar()};
 
 function menuMostrar() {
     if (menu.classList.contains("menu")) {
-        menu.classList.add("menu-mostrar");
-        menu.classList.remove("menu");
+        menu.classList.toggle("menu-mostrar");
     } 
     else if (menu.classList.contains("menu-mostrar")) {
-        menu.classList.add("menu");
-        menu.classList.remove("menu-mostrar");
+        menu.classList.toggle("menu");
+    }
+}
+
+function fixar() {
+    if(menu.classList.contains("menu") && document.documentElement.scrollTop > 150) {
+        menu.classList.add("fixo");
+    } 
+    else {
+        menu.classList.remove("fixo");
     }
 }
