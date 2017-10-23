f2c359b4429f45a271a21c48ec277c9b814ac0e3
Gustavo Oliveira <gusttavo@live.com>
2017-10-22 22:58:53 -0200

    adaptando aos browsers

--- js/app.js
+++ js/app.js
@@ -2,9 +2,11 @@ var menu = document.querySelector(".menu");
 
 function menuMostrar() {
     if (menu.classList.contains("menu")) {
-        menu.classList.toggle("menu-mostrar");
+        menu.classList.add("menu-mostrar");
+        menu.classList.remove("menu");
     } 
     else if (menu.classList.contains("menu-mostrar")) {
-        menu.classList.toggle("menu");
+        menu.classList.add("menu");
+        menu.classList.remove("menu-mostrar");
     }
 }
